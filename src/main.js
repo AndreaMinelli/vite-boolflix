42cea589f9720271c5b8c31bdb7041cd6560e310
AndreaMinelli <minelliandrea94@gmail.com>
2023-01-26 17:52:58 +0100

    Installato FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar as fasFaStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as farFaStar } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(fasFaStar, farFaStar);
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
