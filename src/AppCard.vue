f604a3208cfef93e59fc533ed715e8214ec237fd
AndreaMinelli <minelliandrea94@gmail.com>
2023-01-26 16:32:53 +0100

    Inserite bandiere per la lingua it e us

--- src/AppCard.vue
+++ src/AppCard.vue
@@ -1,4 +1,5 @@
 <script>
+import { langFlag } from "./components/data";
 export default {
   name: "AppCard",
   props: {
@@ -7,6 +8,17 @@ export default {
     language: String,
     vote: Number,
   },
+  computed: {
+    langCheck() {
+      return langFlag.includes(this.language);
+    },
+  },
+  methods: {
+    buildFlagImageUrl(flag) {
+      return new URL(`./components/img/flags/${flag}.png`, import.meta.url)
+        .href;
+    },
+  },
 };
 </script>
 
@@ -14,7 +26,13 @@ export default {
   <div>
     <p>{{ title }}</p>
     <p>{{ originalTitle }}</p>
-    <p>{{ language }}</p>
+    <figure v-if="langCheck" class="w-25">
+      <img
+        :src="buildFlagImageUrl(language)"
+        :alt="language"
+        class="img-fluid" />
+    </figure>
+    <p v-else class="text-uppercase">{{ language }}</p>
     <p>{{ vote }}</p>
   </div>
 </template>
--- src/components/data/index.js
+++ src/components/data/index.js
@@ -1,4 +1,5 @@
 const dbMovieUri = "https://api.themoviedb.org/3";
 const apiKey = "9d2c9c2fc783a64d569f5996a25f85aa";
+const langFlag = ["en", "it"];
 
-export { dbMovieUri, apiKey };
+export { dbMovieUri, apiKey, langFlag };
